a2ae9cd6611a88967c9aa6675d3e72d878058dd6
souravangra <angrasourav@yahoo.com>
2017-07-02 22:36:41 +0530

    King Travel : Latest Code

---
+++ KingTravel/KingTravel_Common/Common.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingTravel_Common
+{
+    public enum ResponseType
+    {
+        INFO,
+        PRIMARY,
+        SUCCESS,
+        WARNING,
+        DANGER,
+        DEFAULT
+    }
+    public enum Verbs
+    {
+        GET,
+        POST
+    }
+
+    class Common
+    {
+    }
+}
